1af223f081ad73083d704fa67d757f9fb0e459d0
ameer akech <ameerakech@gmail.com>
2025-09-15 10:20:29 +0300

    created a sidebar

---
+++ fruitguard/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Sidebar from "./sharedComponents/Sidebar";
+// import Navbar from "./sharedComponents/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+   <Sidebar/>
+   {/* <Navbar/> */}
+    </div>
+  );
+}
